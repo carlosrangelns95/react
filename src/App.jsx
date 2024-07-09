0b1bfb1c0fe1ff28a0163576d09699d1f53e1fe4
Carlos Rangel de Sousa Nunes <carlos.rangel.ns95@gmail.com>
2024-07-09 20:03:58 -0300

    aula 06

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-
+import { Post } from "./Post"
 function App() {
 
   return (
     <>
       <h1>hello, world!</h1>
+      <Post 
+        author="Carlos Rangel"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis necessitatibus at corporis adipisci."
+      />
     </>
   )
 }
