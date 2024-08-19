82bc56cdb7a1ed03b92b01466339803cee5271ab
Carlos Rangel de Sousa Nunes <carlos.rangel.ns95@gmail.com>
2024-08-19 17:02:25 -0300

    Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,38 @@ import { Sidebar } from "./components/Sidebar"
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/166733735?v=4',
+      name: 'Carlos Rangel',
+      role: 'Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW return.' },
+      { type: 'link', content: 'carlos.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/166733735?v=4',
+      name: 'Francisco Rafael',
+      role: 'Chef',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera' },
+      { type: 'paragraph', content: 'Acabei de fazer mais uma pizza. Venham experimentar!' },
+      { type: 'link', content: 'rafael.design/doctorcare' }
+    ],
+    publishedAt: new Date('2024-08-19 16:40:00')
+  },
+]
+
 function App() {
 
   return (
@@ -15,8 +47,17 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {
+            posts.map(post => {
+              return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,27 +1,44 @@
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
 
-export function Post() {
+
+export function Post(props) {
+  const publishedDateFormatted = format(props.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(props.publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src='https://avatars.githubusercontent.com/u/166733735?v=4' />
+          <Avatar src={props.author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Carlos Rangel</strong>
-            <span>Web Developer</span>
+            <strong>{props.author.name}</strong>
+            <span>{props.author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado a 1h</time>
+        <time title={publishedDateFormatted} dateTime={props.publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galera!</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW return.</p>
-        <p><a href="">carlos.design/doctorcare</a></p>
-        <p><a href="">#novoprojeto</a></p>
+        {props.content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else {
+            return <p><a>{line.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
